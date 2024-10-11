1aef612f56e89efb03e3b1a01f577638ed86a382
bulleux <0786623352@orange.fr>
2024-10-11 21:59:18 +0200

    xls Ok

---
+++ output.py
@@ -0,0 +1,130 @@
+import json
+
+import pandas as pd
+from openpyxl import Workbook
+from openpyxl.styles import Border, Side, PatternFill, Font
+
+
+def create_excel_file(data_liste):
+    # Création d'un nouveau fichier Excel
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Commande"
+
+    for data in data_liste:
+
+        built_tab(data, ws)
+
+    # Définir la largeur des colonnes
+    ws.column_dimensions['A'].width = 15  # Largeur pour "N° commande" et "Producteur"
+    ws.column_dimensions['B'].width = 40  # Largeur pour "Nom du client" et "Produit"
+    ws.column_dimensions['C'].width = 3  # Largeur pour "Quantité"
+    ws.column_dimensions['D'].width = 8  # Largeur pour "Prix"
+
+    # Sauvegarde du fichier Excel
+    wb.save("commande_largeur_colonnes_grise.xlsx")
+    print("Fichier Excel avec colonnes ajustées et style créé avec succès !")
+
+
+def built_tab(data, ws):
+    # Définition des bordures
+    thin_border = Border(left=Side(style='thin'),
+                         right=Side(style='thin'),
+                         top=Side(style='thin'),
+                         bottom=Side(style='thin'))
+
+    # Remplissage gris clair
+    gris_fill = PatternFill(start_color="D3D3D3", end_color="D3D3D3", fill_type="solid")
+
+    # Bordure en pointillé gras
+    thick_dotted_border = Border(top=Side(style='mediumDashDot'))
+
+    # Ligne 1 : En-têtes pour N° commande et Nom du client
+    ws.append(["N° commande", "Nom du client"])
+
+    # Appliquer le remplissage gris clair à la ligne des en-têtes
+    for cell in ws[ws.max_row]:
+        cell.fill = gris_fill
+
+    # Ligne 2 : Valeurs pour le numéro de commande et le nom du client
+    ws.append([data["command"]["numero"], data["command"]["client"]])
+
+    # Appliquer le remplissage gris clair à la ligne des données de commande et client
+    for cell in ws[ws.max_row]:
+        cell.fill = gris_fill
+
+    # Ligne 3 : En-têtes des colonnes des produits (Producteur, Produit, Quantité, Prix)
+    ws.append(["Producteur", "Produit", "Qt", "Prix"])
+
+    # Lignes suivantes : Données des produits
+    for produit in data["command"]["produits"]:
+        ws.append([
+            produit["producteur"],
+            produit["produit"],
+            produit["quantite"],
+            produit["prix"]
+        ])
+
+    # Calcul du total
+    total = sum([float(p["prix"]) for p in data["command"]["produits"]])
+
+    # Insertion de la ligne Total avec le calcul à la dernière colonne
+    ws.append(["Total", "", "", total])
+
+    # Récupérer la cellule contenant "Total" (ligne précédente, colonne 1)
+    total_label_cell = ws.cell(row=ws.max_row, column=1)
+
+    # Appliquer la mise en forme gras à la cellule contenant "Total"
+    total_label_cell.font = Font(bold=True)
+
+    # Récupérer la cellule contenant le total (ligne précédente, colonne 4)
+    total_value_cell = ws.cell(row=ws.max_row, column=4)
+
+    # Appliquer la mise en forme gras à la cellule contenant le total
+    total_value_cell.font = Font(bold=True)
+
+    # Appliquer des bordures à chaque cellule du tableau
+    for row in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=4):
+        for cell in row:
+            cell.border = thin_border
+
+    # Ajouter deux lignes vides avec une bordure en pointillé gras entre elles
+    ws.append([""])  # Première ligne vide
+    ws.append([""])  # Deuxième ligne vide
+
+
+def create_producer_price_table(df_achat, file_name='producteurs_prix.xlsx'):
+    df_achat['prix'] = pd.to_numeric(df_achat['prix'], errors='coerce')
+    total_par_producteur = df_achat.groupby('producteur')['prix'].sum().reset_index()
+    liste_totaux = total_par_producteur.to_dict(orient='records')
+    # Créer un nouveau fichier Excel
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Producteurs et Prix"
+
+    # Définir les en-têtes des colonnes
+    ws.append(["Producteur", "Prix"])
+
+    # Définir des bordures fines pour les cellules
+    thin_border = Border(left=Side(style='thin'),
+                         right=Side(style='thin'),
+                         top=Side(style='thin'),
+                         bottom=Side(style='thin'))
+
+    # Ajouter les données dans le tableau
+    for item in liste_totaux:
+        ws.append([item['producteur'], item['prix']])
+
+    # Appliquer des bordures à chaque cellule
+    for row in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=2):
+        for cell in row:
+            cell.border = thin_border
+
+    # Ajuster la largeur des colonnes
+    ws.column_dimensions['A'].width = 30  # Largeur pour "Producteur"
+    ws.column_dimensions['B'].width = 15  # Largeur pour "Prix"
+
+    # Sauvegarder le fichier Excel
+    wb.save(file_name)
+    print(f"Tableau enregistré dans le fichier {file_name} avec succès !")
+
---
+++ services.py
@@ -0,0 +1,130 @@
+from datetime import datetime, timedelta
+
+from woocommerce import API
+import pandas as pd
+from login import login
+from output import create_excel_file, create_producer_price_table
+
+# Informations de connexion à votre boutique WooCommerce
+
+#creer les fichier avec les credentials
+
+wcapi = API(
+    url=login['url'],
+    consumer_key=login['consumer_key'],
+    consumer_secret=login['consumer_secret'],
+    version=login['version']
+)
+df_achat = pd.DataFrame(columns=['order_id', 'produit', 'quantite', 'producteur', 'prix'])
+
+data = {'search': 'chou'}
+
+
+def get_list_de_vendeurs():
+    """
+    on recupère la liste des vendeurs
+    avec le role seller
+    sans l'id 10 de jean nicolas
+    on retourne un liste de producteurs
+    [
+    {id, first_name, username}
+        ]
+    """
+    sellers = wcapi.get("customers", params={'role': 'seller', 'filter[limit]': -1}, ).json()
+    df = pd.DataFrame(sellers)
+    seller_df = df[['id', 'first_name', 'username']]
+    seller_list = seller_df.to_dict(orient='records')
+    return seller_list
+
+
+def obtenir_dernier_dimanche():
+    # Obtenir la date actuelle
+    aujourd_hui = datetime.now()
+
+    # Calculer la différence de jours pour atteindre le dernier dimanche
+    dernier_dimanche = aujourd_hui - timedelta(days=aujourd_hui.weekday() + 1)
+
+    # Si aujourd'hui est déjà un dimanche, revenir à la semaine précédente
+    if aujourd_hui.weekday() == 6:
+        dernier_dimanche = aujourd_hui - timedelta(days=7)
+
+    return dernier_dimanche.strftime('%Y-%m-%dT00:00:00')
+
+
+# Utilisation de la fonction
+print("Dernier dimanche :", obtenir_dernier_dimanche())
+
+def retrieve_main_order():
+    """
+    on recupère les commandes non validées (on-hold)
+    et on return une liste d'id de mini commande
+    """
+
+    response = wcapi.get("orders",
+                         params={'status': 'on-hold', 'parent': 0, 'after': obtenir_dernier_dimanche(), 'per_page': 100})
+    orders_df = pd.DataFrame(response.json())
+    id_list = orders_df['id'].tolist()
+    return id_list
+
+
+#on recupère la liste des vendeurs
+# vendeur_list = get_list_de_vendeurs()
+# id_list = retrieve_main_order()
+# # [1629, 1625, 1621, 1615, 1612, 1605, 1599, 1593, 1577, 1532]
+
+
+def trouver_vendeur_par_id(vendeurs, id_recherche):
+    for vendeur in vendeurs:
+        if vendeur['id'] == id_recherche:
+            return vendeur['first_name']
+    return None  # Si l'id n'est pas trouvé, retourner None
+
+
+def get_order_by_customer_by_vendor(parent):
+
+    response = wcapi.get("orders", params={'parent': parent, 'per_page': 100})
+    orders_df = pd.DataFrame(response.json())
+    # Filtrer le DataFrame pour ne garder que les lignes où parent_id est égal à 0
+
+    # Sélectionner les colonnes spécifiques
+    result_df = orders_df[['id', 'billing', 'meta_data','line_items', 'total']]
+
+    line_items_list = []
+    billing = {}
+    for index, row in result_df.iterrows():
+        billing = row['billing']
+        for item in row['line_items']:
+            ligne = {
+                'order_id': row['id'],
+                'produit': item['name'],
+                'quantite': item['quantity'],
+                'prix': (float(item['subtotal']) + float(item['subtotal_tax'])),
+                'producteur' :  trouver_vendeur_par_id(vendeur_list, int(row['meta_data'][0]['value']))
+            }
+            line_items_list.append(ligne)
+    line_items_list.sort(key=lambda x: x['producteur'])
+    return line_items_list, billing
+
+if __name__ == '__main__':
+    df_achat = pd.DataFrame(columns=['order_id', 'produit', 'quantite', 'producteur', 'prix'])
+    vendeur_list = get_list_de_vendeurs()
+    id_list = retrieve_main_order()
+    # [1629, 1625, 1621, 1615, 1612, 1605, 1599, 1593, 1577, 1532]
+    command_list =[]
+    for parent in retrieve_main_order():
+        command = get_order_by_customer_by_vendor(parent)
+        product = command[0]
+        client = command[1]
+        df_temp = pd.DataFrame(product)
+        data_tab = {'command' :
+                        {
+                            'numero': parent,
+                            'client': f'{client["first_name"]} {client["last_name"]}',
+                            'produits': product
+                         }
+        }
+        command_list.append(data_tab)
+        df_achat = pd.concat([df_achat, df_temp], ignore_index=True)
+    create_excel_file(command_list)
+    create_producer_price_table(df_achat)
+    print("test")
